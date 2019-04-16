38c85dd2b23c95b804d1f79435b76b457256003e
Brian Dodge <bdodge09@gmail.com>
2019-04-15 22:19:29 -0400

    quiet debug down

--- bstreamio/bstreamtls.c
+++ bstreamio/bstreamtls.c
@@ -82,7 +82,7 @@ tlsctx_t;
 
 /// set this from 0 to 5 to get debug output on console. 0 is none, 5 is verbose
 ///
-#define TLS_DEBUG_LEVEL 4
+#define TLS_DEBUG_LEVEL 1
 
 static void tls_log(int level, const char *fmt, ...)
 {
